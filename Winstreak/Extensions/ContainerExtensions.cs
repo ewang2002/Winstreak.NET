a005de9a25444d6b00487c0c51c3d7394341225c
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2020-10-03 10:03:39 -0700

    started working on friends again

--- Winstreak/Extensions/ContainerExtensions.cs
+++ Winstreak/Extensions/ContainerExtensions.cs
@@ -1,21 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Winstreak.Extensions
 {
-	public static class EnumerableExtensions
-	{
-		/// <summary>
-		/// Essentially the ToString method, but shows all elements. 
-		/// </summary>
-		/// <typeparam name="T">The object type.</typeparam>
-		/// <param name="list">The list of objects.</param>
-		/// <returns>All the string-represented elements in the array.</returns>
-		public static string ToReadableString<T>(this IEnumerable<T> list)
-			=> $"[{string.Join(", ", list.ToArray())}]";
-		
-	}
-
 	public static class ArrayExtensions
 	{
 		/// <summary>
